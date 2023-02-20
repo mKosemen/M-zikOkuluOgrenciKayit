def1e07a9d9d20429adfd9529c2cebe05ce9cc04
mKosemen <103363649+mKosemen@users.noreply.github.com>
2023-02-20 22:17:33 +0300

    Add project files.

---
+++ MüzikOkuluOgrenciKayit/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MüzikOkuluOgrenciKayit
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            DataSetTableAdapters.ÖğrencilerTableAdapter dt = new DataSetTableAdapters.ÖğrencilerTableAdapter();
+            dt.StudentAdd(TextBox1.Text, TextBox3.Text, TextBox4.Text, DropDownList1.SelectedItem.ToString(), DropDownList2.SelectedItem.ToString());
+
+        }
+    }
+}
